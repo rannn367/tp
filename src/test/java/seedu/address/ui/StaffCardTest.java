eeb7ad6eb1234694e7098a824673d5a60da456c5
WhiteBear82 <whitebearr82@gmail.com>
2025-03-07 21:30:24 +0800

    Add Tests Staff

--- src/test/java/seedu/address/ui/StaffCardTest.java
+++ src/test/java/seedu/address/ui/StaffCardTest.java
@@ -1,22 +1,23 @@
 package seedu.address.ui;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static seedu.address.testutil.TypicalStaff.ALEX;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.person.Staff;
-import seedu.address.testutil.StaffBuilder;
+import javafx.application.Platform;
 
 public class StaffCardTest {
+    @BeforeAll
+    static void setupJavaFX() {
+        Platform.startup(() -> {}); // Ensure JavaFX initializes once before tests run
+    }
 
     @Test
     public void createStaffCard_validStaff_success() {
-        Staff staff = new StaffBuilder().withName("Alice Tan").withPhone("91234567")
-                .withEmail("alice@example.com").withStaffId("S123")
-                .withRole("Manager").withShiftTiming("Morning")
-                .withHoursWorked(40).withPerformanceRating(5).build();
 
-        StaffCard staffCard = new StaffCard(staff, 1);
+        StaffCard staffCard = new StaffCard(ALEX, 1);
 
         assertNotNull(staffCard);
     }
