1158ec69743b6f6c98b8c389f355c77ea3e38c96
WhiteBear82 <whitebearr82@gmail.com>
2025-03-15 18:34:31 +0800

    Add GUI background

--- src/main/java/seedu/address/ui/StaffListPanel.java
+++ src/main/java/seedu/address/ui/StaffListPanel.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.application.ColorScheme;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
