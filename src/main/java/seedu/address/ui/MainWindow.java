bedd4664c4f1967a571c8dc9719910f788fe534f
WhiteBear82 <whitebearr82@gmail.com>
2025-03-15 17:39:42 +0800

    Display columns hover effect

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -64,6 +64,7 @@ public class MainWindow extends UiPart<Stage> {
     public MainWindow(Stage primaryStage, Logic logic) {
         super(FXML, primaryStage);
 
+
         // Set dependencies
         this.primaryStage = primaryStage;
         this.logic = logic;
