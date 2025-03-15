7d656904965a3f43859da7e732a213556c885a0e
WhiteBear82 <whitebearr82@gmail.com>
2025-03-15 19:28:44 +0800

    Change display information formatting GUI

--- src/main/java/seedu/address/ui/StaffCard.java
+++ src/main/java/seedu/address/ui/StaffCard.java
@@ -9,6 +9,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Staff;
 
+/**
+ * An UI component that displays information of a {@code Staff}.
+ */
 public class StaffCard extends UiPart<Region> {
 
     private static final String FXML = "StaffListCard.fxml";
@@ -38,6 +41,9 @@ public class StaffCard extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+    /**
+     * Creates a {@code StaffCard} with the given {@code Staff} and index to display.
+     */
     public StaffCard(Staff staff, int displayedIndex) {
         super(FXML);
         this.staff = staff;
