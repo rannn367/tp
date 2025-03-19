2e615cc1d33572fa18a570c9a5173c3727a9974a
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-03-19 10:43:16 +0000

    Add suggestion for incorrect commands

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,6 +6,12 @@ import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddCommand;
@@ -33,12 +39,40 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses user input.
  */
 public class AddressBookParser {
+    
+    @FunctionalInterface
+    interface ThrowingFunction<T, R, E extends Exception> {
+        R apply(T t) throws E;
+    }
 
     /**
      * Used for initial separation of command word and args.
      */
     private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
     private static final Logger logger = LogsCenter.getLogger(AddressBookParser.class);
+
+    private static final Map<String, ThrowingFunction<String, Command, ParseException>> COMMAND_MAP = new HashMap<>();
+
+    static {
+        COMMAND_MAP.put(AddCommand.COMMAND_WORD, arguments -> new AddCommandParser().parse(arguments));
+        COMMAND_MAP.put(EditCommand.COMMAND_WORD, arguments -> new EditCommandParser().parse(arguments));
+        COMMAND_MAP.put(DeleteCommand.COMMAND_WORD, arguments -> new DeleteCommandParser().parse(arguments));
+        COMMAND_MAP.put(ClearCommand.COMMAND_WORD, arguments -> new ClearCommand());
+        COMMAND_MAP.put(FindCommand.COMMAND_WORD, arguments -> new FindCommandParser().parse(arguments));
+        COMMAND_MAP.put(RemarkCommand.COMMAND_WORD, arguments -> new RemarkCommandParser().parse(arguments));
+        COMMAND_MAP.put(ListCommand.COMMAND_WORD, arguments -> new ListCommand());
+        COMMAND_MAP.put(ExitCommand.COMMAND_WORD, arguments -> new ExitCommand());
+        COMMAND_MAP.put(HelpCommand.COMMAND_WORD, arguments -> new HelpCommand());
+        COMMAND_MAP.put(AddStaffCommand.COMMAND_WORD, arguments -> new AddStaffCommandParser().parse(arguments));
+        COMMAND_MAP.put(EditStaffCommand.COMMAND_WORD, arguments -> new EditStaffCommandParser().parse(arguments));
+        COMMAND_MAP.put(DeleteStaffCommand.COMMAND_WORD, arguments -> new DeleteStaffCommandParser().parse(arguments));
+        COMMAND_MAP.put(AddCustomerCommand.COMMAND_WORD, arguments -> new AddCustomerCommandParser().parse(arguments));
+        COMMAND_MAP.put(DeleteCustomerCommand.COMMAND_WORD, arguments -> new DeleteCustomerCommandParser().parse(arguments));
+        COMMAND_MAP.put(PointsAddCommand.COMMAND_WORD, arguments -> new PointsAddCommandParser().parse(arguments));
+        COMMAND_MAP.put(DrinkAddCommand.COMMAND_WORD, arguments -> new DrinkAddCommandParser().parse(arguments));
+        COMMAND_MAP.put(PurchaseCommand.COMMAND_WORD, arguments -> new PurchaseCommandParser().parse(arguments));
+        COMMAND_MAP.put(HoursAddCommand.COMMAND_WORD, arguments -> new HoursAddCommandParser().parse(arguments));
+    }
 
     /**
      * Parses user input into command for execution.
@@ -61,62 +95,89 @@ public class AddressBookParser {
         // Lower level log messages are used sparingly to minimize noise in the code.
         logger.fine("Command word: " + commandWord + "; Arguments: " + arguments);
 
-        switch (commandWord) {
-
-        case AddCommand.COMMAND_WORD:
-            return new AddCommandParser().parse(arguments);
-
-        case EditCommand.COMMAND_WORD:
-            return new EditCommandParser().parse(arguments);
-
-        case DeleteCommand.COMMAND_WORD:
-            return new DeleteCommandParser().parse(arguments);
-
-        case ClearCommand.COMMAND_WORD:
-            return new ClearCommand();
-
-        case FindCommand.COMMAND_WORD:
-            return new FindCommandParser().parse(arguments);
-
-        case RemarkCommand.COMMAND_WORD:
-            return new RemarkCommandParser().parse(arguments);
-
-        case ListCommand.COMMAND_WORD:
-            return new ListCommand();
-
-        case ExitCommand.COMMAND_WORD:
-            return new ExitCommand();
-
-        case HelpCommand.COMMAND_WORD:
-            return new HelpCommand();
-
-        case AddStaffCommand.COMMAND_WORD:
-            return new AddStaffCommandParser().parse(arguments);
-
-        case EditStaffCommand.COMMAND_WORD:
-            return new EditStaffCommandParser().parse(arguments);
-
-        case DeleteStaffCommand.COMMAND_WORD:
-            return new DeleteStaffCommandParser().parse(arguments);
-
-        case AddCustomerCommand.COMMAND_WORD:
-            return new AddCustomerCommandParser().parse(arguments);
-        case DeleteCustomerCommand.COMMAND_WORD:
-            return new DeleteCustomerCommandParser().parse(arguments);
-        case PointsAddCommand.COMMAND_WORD:
-            return new PointsAddCommandParser().parse(arguments);
-        case DrinkAddCommand.COMMAND_WORD:
-            return new DrinkAddCommandParser().parse(arguments);
-        case PurchaseCommand.COMMAND_WORD:
-            return new PurchaseCommandParser().parse(arguments);
-        case HoursAddCommand.COMMAND_WORD:
-            return new HoursAddCommandParser().parse(arguments);
-
-
-        default:
-            logger.finer("This user input caused a ParseException: " + userInput);
-            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+        if (COMMAND_MAP.containsKey(commandWord)) {
+            return COMMAND_MAP.get(commandWord).apply(arguments);
         }
+
+        String closestCommand = findClosestCommand(commandWord);
+        if (closestCommand != null) {
+            throw new ParseException(String.format("Unknown command: '%s'. Did you mean '%s'?", commandWord, closestCommand));
+        }
+
+        throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
     }
 
+    private String findClosestCommand(String inputCommand) {
+        int minDistance = Integer.MAX_VALUE;
+        String closestCommand = null;
+
+        for (String command : COMMAND_MAP.keySet()) {
+            int distance = calculateLevenshteinDistance(inputCommand, command);
+
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestCommand = command;
+            }
+        }
+
+        if (minDistance <= 2) {
+            return closestCommand;
+        }
+
+        // Use TokenSetRatio if LeveischteinDistance fails.
+        int bestTokenSetRatio = 0;
+        for (String command : COMMAND_MAP.keySet()) {
+            int tokenSetRatio = calculateTokenSetRatio(inputCommand, command);
+
+            if (tokenSetRatio > bestTokenSetRatio) {
+                bestTokenSetRatio = tokenSetRatio;
+                closestCommand = command;
+            }
+        }
+
+        if (bestTokenSetRatio >= 80) {
+            return closestCommand;
+        }
+
+        return null;
+    }
+
+    private int calculateTokenSetRatio(String a, String b) {
+        Set<Character> tokensA = a.chars()
+                .mapToObj(e -> (char) e)
+                .collect(Collectors.toCollection(HashSet::new));
+        Set<Character> tokensB = b.chars()
+                .mapToObj(e -> (char) e)
+                .collect(Collectors.toCollection(HashSet::new));
+
+        Set<Character> intersection = tokensA.stream()
+            .filter(tokensB::contains)
+            .collect(Collectors.toSet());
+
+        Set<Character> union = new HashSet<>(tokensA);
+        union.addAll(tokensB);
+
+        return (int) ((double) intersection.size() / union.size() * 100);
+    }
+
+    private int calculateLevenshteinDistance(String a, String b) {
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = 0; i <= a.length(); i++) {
+            for (int j = 0; j <= b.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = Math.min(
+                        dp[i - 1][j - 1] + (a.charAt(i - 1) == b.charAt(j - 1) ? 0 : 1),
+                        Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+                    );
+                }
+            }
+        }
+
+        return dp[a.length()][b.length()];
+    }
 }
